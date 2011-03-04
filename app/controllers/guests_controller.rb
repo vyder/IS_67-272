9af2ef18f500deabe10c90ee24c480aa0a5376aa
Vidur Murali <vidur.murali@gmail.com>
2011-03-03 23:43:21 -0500

    Minor tweaks to titles

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -4,7 +4,7 @@ class GuestsController < ApplicationController
 
   def index
     @guests = Guest.all.paginate :page => params[:page], 
-																 :per_page => 15
+																 :per_page => 12
   end
 
   def show
