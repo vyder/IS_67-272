2d7bc9e6f27ceb3a78b420c934d790c00b1438b8
Vidur Murali <vidur.murali@gmail.com>
2011-05-02 07:44:55 -0400

    Base mailer created

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -1,8 +1,8 @@
-ActionMailer::Base.smtp_settings = {  
-  :address              => "smtp.gmail.com",  
-  :port                 => 587,  
-  :user_name            => "lheimann",  
-  :password             => "fredhead-404",  
-  :authentication       => "plain",  
-  :enable_starttls_auto => true  
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => "partymanager.mailer",
+  :password             => "totallySecret",
+  :authentication       => "plain",
+  :enable_starttls_auto => true
 }
