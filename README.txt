8a5373c2d35aaff500bb8c9bfaf37b60dadd4ae1
Vidur Murali <vidur.murali@gmail.com>
2011-05-04 00:00:27 -0400

    Completed Unit Tests

--- README.txt
+++ README.txt
@@ -1,3 +1,22 @@
-1. "Invited Guests" is changed to "Guest List"
+--------------------------------------------------------------------
+### ReadMe File - @author: Vidur Murali (vidur.murali@gmail.com) ###
+--------------------------------------------------------------------
 
-2. "Name: <guest_name>" changed to "<guest_name> | Guest Details"
+Hello TA,
+
+This Readme file has been split into sections for easy reading.
+
+Extra Credit I have implemented:
+(Note: Items that have a * are ones that have been suggested/approved by ProfH. ~ signifies I didn't have time to get them cleared by ProfH, so you should use your discretion to give me extra credit :) Thanks! )
+
+1) *Dynamic Maps - coded from scratch in javascript (not the iframe crap) using the google maps API
+		> ~Implements zoom using the scroll wheel, and UI
+		> ~Street View (drag the little guy to your location)
+			> ~Click on marker to open infoWindow
+			> ~infoWindow has a link to get directions location
+		> ~Option to change to Satellite or Map view
+
+2) ~ Home page lists only the next 5 upcoming parties
+
+Thanks,
+Vidur
--- test/unit/party_test.rb
+++ test/unit/party_test.rb
@@ -1,37 +1,75 @@
 require 'test_helper'
 
 class PartyTest < ActiveSupport::TestCase
-#  def test_should_be_valid
-#    assert Party.new.valid?
-#  end
+
+	## Testing Relationships ##
+  should belong_to(:host)
+	should belong_to(:party_type)
+	should belong_to(:location)
+  should have_many(:invitations)
+	should have_many(:guests).through(:invitations)
+  
+	## Testing Validations ##
+  should validate_presence_of(:name)
+  should validate_presence_of(:host_id)
+  should validate_presence_of(:party_date)
+  should validate_presence_of(:location_id)
+  should validate_presence_of(:start_time)
+  should validate_presence_of(:end_time)
 
 	# -------------------------------------------------
-  # More shoulda testing for the other methods, etc.
-  # by setting up context(s)
-  #
+	# Testing other methods by setting up a context
+	
+	context "Creating two parties for one host" do
+		# create the objects I want with factories
+		setup do 
+			@host = Host.new
+			@host.id = 1
 
-	# ------------------------------------------------------------------
-  # Context to test confirmed and expected guest calculation functions
-#	context "Party evaluates number of guests correctly" do
-#		setup do
-#			@party = Factory.create(:party)
-#		end
+			@party1 = Party.new
+			@party1.name = "Birthday"
+			@party1.host_id = @host.id
 
-#		teardown do
-#			@party.destroy
-#		end
+#			@party2 = Party.new
+#			@party2.name = "Graduation"
+#			@party2.host_id = @host.id
 
-		# now run the tests:
+			@party2 = Factory.create(:party, :host_id => @host.id, :name => "Graduation")
+		end
+		
+		# and provide a teardown method as well
+		teardown do
+			@party2.destroy
+			@party1.destroy
+			@host.destroy
+		end
 
-		# test the counting of confirmed guests
-#		should "shows that party has 0 confirmed guests" do
-#			assert_equal 0, @party.confirmed
-#		end
+		## Testing Creation/Save ##
 
-		# test the counting of expected guests
-#		should "shows that party has 0 expected guests" do
-#			assert_equal 0, @party.expected
-#		end
-#	end
+		# Checks that the parties have the right data by checking one of them
+		should "show that the parties is created properly" do
+			assert_equal "Bob", @guest1.name
+			assert_equal "bob@example.com", @guest1.email
+		end
+
+		## Testing Scopes ##
+
+		# scope :all
+		should "show that all the guest are listed alphabetically" do
+			assert_equal 3, Guest.all.size
+			assert_equal ["Bob", "Jack", "Mark"], Guest.all.map{|g| g.name}
+		end
+	
+		# scope :for_host(host_id)
+		should "have scope for_host that works" do
+			assert_equal 2, Guest.for_host(1).size
+			assert_equal 1, Guest.for_host(2).size
+		end
+
+		## Testing Callbacks ## - none to test
+
+		## Testing Methods ## - none to test
+
+	end
 
 end
